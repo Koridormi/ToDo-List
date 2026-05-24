3827cd6374765e5480acdd3aef90a37be13a436d
Koridormi <258958198+Koridormi@users.noreply.github.com>
2026-05-24 03:36:27 -0300

    first commit

---
+++ to-do_list/gulpfile.js
@@ -0,0 +1,16 @@
+import gulpSass from 'gulp-sass';
+import * as dartSass from 'sass';
+import {src, dest, watch} from 'gulp';
+
+const sass = gulpSass(dartSass);
+
+export function css(done) {
+    src('./src/scss/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('./build/css'));
+    done();
+};
+
+export function dev() {
+    watch('./src/scss/**/*.scss', css);
+};

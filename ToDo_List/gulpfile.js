272a0bb353b5775db16a05418ecbabf6538845a3
Koridormi <258958198+Koridormi@users.noreply.github.com>
2026-06-20 08:02:11 -0300

    Update Structure

---
+++ ToDo_List/gulpfile.js
@@ -0,0 +1,31 @@
+import gulpSass from 'gulp-sass';
+import {src, dest, watch, series} from 'gulp';
+import * as dartSass from 'sass';
+import terser from 'gulp-terser';
+
+const sass = gulpSass(dartSass);
+
+export function js(done) {
+    src('./src/js/app.js')
+        .pipe(terser())
+        .pipe(dest('build/js'));
+
+    done();
+};
+
+export function css(done) {
+    src('./src/scss/style.scss', {sourcemaps: true})
+        .pipe(sass({
+            style: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(dest('./build/css', {sourcemaps: '.'}));
+
+    done();
+};
+
+export function dev() {
+    watch('./src/scss/**/*.scss', css)
+    watch('./src/js/**/*.js', js);
+};
+
+export default series(js, css, dev);
